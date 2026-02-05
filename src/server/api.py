117b230fe32efbc2354a49c663479e300d6a3ec1
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2026-02-05 06:15:28 +0530

    Update api.py

--- src/server/api.py
+++ src/server/api.py
@@ -10,11 +10,6 @@ import os
 from src.extractor.mediapipe_extractor import extract_pose_from_video
 from src.features.gait_features import extract_gait_features
 
-# from src.models.synthetic_pressure import generate_pressure_map
-# from src.models.cnn_model import predict_load_from_pressure
-# from src.models.prescription import load_to_prescription
-
-
 app = FastAPI(title="VORMA Prototype API")
 
 app.add_middleware(
@@ -57,49 +52,3 @@ async def analyze_video(file: UploadFile = File(...)):
         "num_frames": int(keypoints.shape[0]),
         "gait_features": gait_features
     })
-
-
-# @app.post("/api/analyze")
-# async def analyze_video(file: UploadFile = File(...)):
-#     """
-#     1. Save uploaded video
-#     2. Run Mediapipe extractor -> keypoints.npy
-#     3. Run simple model -> height map
-#     4. Return JSON with results + basic metadata
-#     """
-#     ext = Path(file.filename).suffix or ".mp4"
-#     vid_id = uuid.uuid4().hex
-
-#     save_path = UPLOAD_DIR / f"{vid_id}{ext}"
-#     with save_path.open("wb") as buffer:
-#         shutil.copyfileobj(file.file, buffer)
-
-#     # Run extractor
-#     out_dir = PROCESSED_DIR / vid_id
-#     json_path, npy_path = extract_pose_from_video(str(save_path), str(out_dir))
-
-#     # Load keypoints
-#     keypoints = np.load(npy_path)  # (T, 33, 4)
-
-#     # 1. Extract gait features
-#     gait_features = extract_gait_features(keypoints)
-
-#     # 2. Generate synthetic plantar pressure map
-#     pressure_map = generate_pressure_map(gait_features)
-
-#     # 3. CNN prediction (plantar load)
-#     predicted_load = predict_load_from_pressure(pressure_map)
-
-#     # 4. Orthotic prescription
-#     prescription = load_to_prescription(predicted_load)
-
-#     response = {
-#         "id": vid_id,
-#         "video_filename": file.filename,
-#         "num_frames": int(keypoints.shape[0]),
-#         "gait_features": gait_features,
-#         "predicted_load": predicted_load,
-#         "orthotic_prescription": prescription
-#     }
-
-#    return JSONResponse(response)
